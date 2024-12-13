03d37a8663431cb71794e68080d9089e0d514d59
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-13 11:11:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// class Animal {
+//   constructor(name, type) {
+//     this.name = name;
+//     this.type = type;
+//   }
+//   toString() {
+//     return `${this.name} is a ${this.type}`;
+//   }
+// }
+// let dog = new Animal("max", "dog");
+// console.log(dog.toString());
+
+// class Dog {
+//   constructor(name, age, gender, species, size, master, loyal) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.species = species;
+//     this.legs = 4;
+//     this.size = size;
+//     this.master = master;
+//     this.loyal = loyal;
+//   }
+// };
+
+// class Labrador {
+//   constructor(name, age, gender, master) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.species = "Labrador";
+//     this.legs = 4;
+//     this.size = "Large";
+//     this.master = master;
+//     this.loyal = true;
+//   }
+// };
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//  info (){
+//     return `${this.name}s age is ${this.age}`
+//  }
+
+// }
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primery:"#fead04",
+      }
+    },
+  },
+  plugins: [],
+}
